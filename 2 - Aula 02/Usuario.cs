9e575c8d830507a429877e52393c8426155e8a23
Sthefany Lorrany Gomes Silva <2022102020014@aluno.ifro.edu.br>
2024-02-27 15:21:34 -0400

    first commit

---
+++ 2 - Aula 02/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___Aula_02
+{
+    internal class Usuario
+    {
+        public string Nome { get; set; }
+        public DateTime DataNasc { get; set; }
+        public string Cpf { get; set; }
+        public string Genero { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }   
+        public Usuario() { }
+        public Usuario(string nome, DateTime dataNasc, string cpf, string genero, string email, string senha)
+        {
+            Nome = nome;
+            DataNasc = dataNasc;
+            Cpf = cpf;
+            Genero = genero;
+            Email = email;
+            Senha = senha;
+        }
+    }
+}
